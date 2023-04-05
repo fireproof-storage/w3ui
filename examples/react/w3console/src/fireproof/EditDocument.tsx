c967e1baaae78a6549528591df79178176ca1444
Chris Anderson <jchris@gmail.com>
2023-04-05 14:08:15 -0700

    working on table

--- examples/react/w3console/src/fireproof/EditDocument.tsx
+++ examples/react/w3console/src/fireproof/EditDocument.tsx
@@ -2,9 +2,8 @@ import React, { Fragment, useState, useEffect, useContext } from 'react'
 
 // import { FireproofCtx, FireproofCtxValue } from '@fireproof/core/hooks/use-fireproof'
 import { FireproofCtx, FireproofCtxValue } from '../../../../../../fireproof/packages/fireproof/hooks/use-fireproof'
-
-import 'jsoneditor-react/es/editor.min.css';
-
+import Highlight, { defaultProps } from 'prism-react-renderer'
+import theme from 'prism-react-renderer/themes/nightOwl'
 
 interface EditDocumentProps {}
 
@@ -30,11 +29,45 @@ export function EditDocument({}: EditDocumentProps): JSX.Element {
     }
     getDocument()
   }, [ready, database, updateCount])
+
+  const docData = {}
+  const docMeta = {}
+  Object.keys(theDocument).forEach(key => {
+    if (key.startsWith('_')) {
+      docMeta[key] = theDocument[key]
+    } else {
+      docData[key] = theDocument[key]
+    }
+  })
+
   return (
     <div class={`bg-slate-800 p-6`}>
-      <h2 class="text-2xl">{theDocument._id}</h2>
-      <pre>{JSON.stringify(theDocument, null, 2)}</pre>
+      <h2 class="text-2xl">_id: {theDocument._id}</h2>
+      <h3>Document data</h3>
+      <CodeHighlight data={docData} theme={theme} />
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-5 rounded">Save</button>
+      <h3>Metadata</h3>
+      <CodeHighlight data={docMeta} theme={defaultProps.theme} />
     </div>
   )
 }
 
+function CodeHighlight({ data, theme }: any) {
+  return (
+    <div class="p-2">
+      <Highlight {...defaultProps} className="p-6" theme={theme} code={JSON.stringify(data, null, 2)} language="json">
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className+' p-2'} style={style}>
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
+    </div>
+  )
+}
--- examples/react/w3console/src/fireproof/ListDocuments.tsx
+++ examples/react/w3console/src/fireproof/ListDocuments.tsx
@@ -64,36 +64,7 @@ export function ListDocuments({ spaces, selected, setSelected, className = '' }:
     <div class={`${className}  bg-slate-800 p-6`}>
       <h2 class="text-2xl">List Documents</h2>
 
-      <div class="relative overflow-x-auto dark">
-        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead class="text-xs text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" class="px-6 py-3">
-                _id
-                <sub class="text-gray-400 dark:text-gray-500">({idCount})</sub>
-              </th>
-              {sortedHeaders.map(([header, count]) => (
-                <th scope="col" class="px-6 py-3">
-                  {header}
-                  <sub class="text-gray-400 dark:text-gray-500">({count})</sub>
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {allDocuments.map(({ _id, ...fields }: any) => (
-              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                  <a href={`/fp-doc?id=${_id}`}>{_id}</a>
-                </th>
-                {sortedHeaders.map(([header]) => (
-                  <td class="px-6 py-4">{formatTableCellContent(fields[header])}</td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <DynamicTable headers={sortedHeaders} />
     </div>
   )
 }
