7be7a074224c786102b848e8463ce08dd53370f2
Chris Anderson <jchris@gmail.com>
2023-04-04 22:52:56 -0700

    actual new code for a new feature

--- examples/react/w3console/src/fireproof/BrowseChanges.tsx
+++ examples/react/w3console/src/fireproof/BrowseChanges.tsx
@@ -9,8 +9,8 @@ interface BrowseChangesProps {}
 export function BrowseChanges({}: BrowseChangesProps): JSX.Element {
   const { ready, database, addSubscriber } = useContext(FireproofCtx) as FireproofCtxValue
   const [updateCount, setUpdateCount] = useState(0)
-  const [allDocuments, setAllDocuments] = useState<any>([])
-  console.log('BrowseChanges', updateCount, allDocuments)
+  const [theChanges, setTheChanges] = useState<any>([])
+  console.log('BrowseChanges', updateCount, theChanges)
   useEffect(() => {
     if (ready && database) {
       addSubscriber('BrowseChanges', () => {
@@ -21,35 +21,35 @@ export function BrowseChanges({}: BrowseChangesProps): JSX.Element {
   useEffect(() => {
     async function getDocuments() {
       if (ready && database) {
-        const allDocs = await database.allDocuments()
-        setAllDocuments(allDocs.rows.map((row: { value: any }) => row.value))
+        const results = await database.changesSince() // todo need head
+        setTheChanges(results.rows)
       }
     }
     getDocuments()
   }, [ready, database, updateCount])
   return (
     <div class={` bg-slate-800 p-6`}>
-      <h2 class="text-2xl">List Documents</h2>
+      <h2 class="text-2xl">Recent changes</h2>
 
       <ul class="max-w-md pt-4 divide-y divide-gray-200 dark:divide-gray-700">
-        {allDocuments.map((doc: any) => (
-          <DocumentListing doc={doc} />
+        {theChanges.map((row: any) => (
+          <ChangeListing row={row} />
         ))}
       </ul>
     </div>
   )
 }
 
-function DocumentListing({ doc: { _id, ...values } }: any): JSX.Element {
+function ChangeListing({ row: { key, value } }: any): JSX.Element {
   return (
-    <li key={_id} class="pt-1 pb-2">
+    <li key={key} class="pt-1 pb-2">
       <div class="flex items-center space-x-4">
         <div class="flex-shrink-0">*</div>
         <div class="flex-1 min-w-0">
           <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-            <a href={`/fp-doc?id=${_id}`}>{_id}</a>
+            <a href={`/fp-doc?id=${key}`}>{key}</a>
           </p>
-          <p class="text-sm text-gray-500 truncate dark:text-gray-400">{JSON.stringify(values)}</p>
+          <p class="text-sm text-gray-500 truncate dark:text-gray-400">{JSON.stringify(value)}</p>
         </div>
       </div>
     </li>
